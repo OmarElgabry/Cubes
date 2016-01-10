3d66ccdc22c121ea6c92393ae31851f4a7c407f1
Omar El Gabry <omar.elgabry.93@gmail.com>
2016-01-10 16:59:41 +0200

    Add other ways to loop on unique numbers
    We can loop on unique numbers either using the set, boolean array, or the array of numbers.

--- src/Other/Unique Numbers.cpp
+++ src/Other/Unique Numbers.cpp
@@ -24,6 +24,15 @@ int main() {
 	printf("Number of unqiue numbers using bool array: %d\n", total);
 	printf("Number of unqiue numbers using set: %d\n", (int)s.size());
 
+	for (unordered_set<int>::iterator it = s.begin(); it != s.end(); it++){
+		printf("%d\n", *it);
+	}
+
+	for (int i = 0; i < MAX; i++){
+		if (unique[i])
+			printf("%d\n", i);
+	}
+
 	for (int i = 0; i < n; i++){
 		if (unique[arr[i]])
 			printf("%d\n", arr[i]), unique[arr[i]] = false;
