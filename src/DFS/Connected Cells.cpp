87a456b7dbad59a6be66300581e43742df4b8976
Omar El Gabry <omar.elgabry.93@gmail.com>
2017-10-01 12:20:44 +0530

    Update Connected Cells.cpp
    No need for count, just return 1 in case you found a connected block

--- src/DFS/Connected Cells.cpp
+++ src/DFS/Connected Cells.cpp
@@ -3,24 +3,26 @@
 using namespace std;
 
 const int n = 4;
-int cunt = 0;
+// int cunt = 0;
 char grid[n][n];
 bool vis[n][n];
 
-void countConnectedCells(int r, int c) {
+/*void*/ bool countConnectedCells(int r, int c) {
 
 	if (r < 0 || r >= n || c < 0 || c >= n || grid[r][c] == 'X' || vis[r][c] == 1) // 'X' means block position
-		return;	
+		return false;	
 
 
 	vis[r][c] = 1;	
-	cunt++;
+	// cunt++;
 
 	// try the 4 neighbor cells
 	countConnectedCells(r, c - 1);  // go left
 	countConnectedCells(r, c + 1);  // go right
 	countConnectedCells(r - 1, c);  // go up
 	countConnectedCells(r + 1, c);  // go down
+	
+	return true;
 }
 
 
@@ -44,11 +46,11 @@ int main(){
 	for (int i = 0; i < n; ++i)	{
 		for (int j = 0; j < n; ++j){
 
-			cunt = 0;
-			countConnectedCells(i, j);
+			// cunt = 0;
+			blocks += countConnectedCells(i, j);
 
-			if (cunt > 0)
-				blocks++;
+			// if (cunt > 0)
+			//	blocks++;
 		}
 	}
 
