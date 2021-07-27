6102c0d680f047f24b7a85b75def6c7d2a6728c3
Omar Elgabry <omar.elgabry.93@gmail.com>
2021-07-27 12:06:42 -0700

    Update Alpha Characters.cpp

--- src/Strings/Alpha Characters.cpp
+++ src/Strings/Alpha Characters.cpp
@@ -12,6 +12,7 @@ int main() {
 	string str = ".omar.is.brilliant.";
 	unordered_set<string> s;
 
+	// METHOD 1: Loop through str, examine it char by char
 	int size = (int)str.size();
 	string ans = "";
 
@@ -29,6 +30,11 @@ int main() {
 	for (it = s.begin(); it != s.end(); it++){
 		cout << *it << endl;
 	}
+	
+	// METHOD 2: Use strings.split(str, ".") = strs
+	// For each s in strs:
+	//	if s is not empty and contains only alpha, then insert it into the set
+	//	otherwise ignore it.
 
 	return 0;
 }
